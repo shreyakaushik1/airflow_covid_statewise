46f2e2d40489cce360ca6a6bbc80704e7b9bd787
shreya1798 <shreyasgnr@gmail.com>
2020-06-05 16:35:52 +0530

    code refactoring

--- dags/covid.py
+++ dags/covid.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+import datetime
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
@@ -6,62 +6,88 @@ import requests
 import pandas as pd
 from google.cloud import bigquery
 from google.oauth2 import service_account
+import logging
 
-def create_csv():
-    resp = requests.get('https://api.covidindiatracker.com/state_data.json')
-    data = resp.json()
+
+def data_to_dataframe(data):
     states = []
     cases = []
     dates = []
-    for d in data:
-        states.append(d['state'])
-        cases.append(d['confirmed'])
-        dates.append(datetime.strftime(datetime.today(), '%d-%m-%Y'))
-    df = {
+    for row in data:
+        states.append(row['state'])
+        cases.append(row['confirmed'])
+        dates.append(datetime.datetime.strftime(datetime.datetime.today(), '%d-%m-%Y'))
+    statewise_data = {
         'states': states,
         'cases': cases,
         'date': dates
     }
-    dff = pd.DataFrame(df)
-    csv_name = 'csv_' + datetime.strftime(datetime.today(), '%d-%m-%Y') + '.csv'
-    dff.to_csv(csv_name)
+    statewise_dataframe = pd.DataFrame(statewise_data)
+    return statewise_dataframe
+
+
+def create_csv():
+    try:
+        api_response = requests.get('https://api.covidindiatracker.com/state_data.json')
+        covid_data = api_response.json()
+        statewise_dataframe = data_to_dataframe(covid_data)
+        csv_name = 'csv_' + datetime.datetime.strftime(datetime.datetime.today(), '%d-%m-%Y') + '.csv'
+        statewise_dataframe.to_csv(csv_name)
+    except requests.exceptions.HTTPError as errh:
+        logging.error("Http Error:", errh)
+    except requests.exceptions.ConnectionError as errc:
+        logging.erorr("Error Connecting:", errc)
+    except requests.exceptions.Timeout as errt:
+        logging.error("Timeout Error:", errt)
+    except requests.exceptions.RequestException as err:
+        logging.error("OOps: Something Else", err)
 
 def to_bigquery():
-    csv_name = 'csv_' + datetime.strftime(datetime.today(), '%d-%m-%Y') + '.csv'
-    data = pd.read_csv(csv_name, index_col=0)
-    print(data.dtypes)
+    csv_name = 'csv_' + datetime.datetime.strftime(datetime.datetime.today(), '%d-%m-%Y') + '.csv'
+    csv_data = pd.read_csv(csv_name, index_col=0)
     credentials = service_account.Credentials.from_service_account_file(
         'bigquery-271709-03f68b4ecacf.json')
     project_id = 'bigquery-271709'
     client = bigquery.Client(credentials=credentials, project=project_id)
-    data.to_gbq(destination_table='covid_dataset.covid_statewise',
+    csv_data.to_gbq(destination_table='covid_dataset.covid_statewise',
                 project_id='bigquery-271709',
                 credentials=credentials,
                 if_exists='append',
                 table_schema=None)
 
 def percentage_upload():
-    csv_name = 'csv_' + datetime.strftime(datetime.today(), '%d-%m-%Y') + '.csv'
-    data = pd.read_csv(csv_name, index_col=0)
-    index = data.index
+    csv_name = 'csv_' + datetime.datetime.strftime(datetime.datetime.today(), '%d-%m-%Y') + '.csv'
+    csv_data = pd.read_csv(csv_name, index_col=0)
+    index = csv_data.index
     number_of_rows = len(index)
     credentials = service_account.Credentials.from_service_account_file(
         'bigquery-271709-03f68b4ecacf.json')
     project_id = 'bigquery-271709'
     sql = 'select count(*) as TotalRows from covid_dataset.covid_statewise_cases'
     client = bigquery.Client(project=project_id, credentials=credentials)
-    df = client.query(sql).to_dataframe()
-    rows_bigquery = df['TotalRows'][0]
+    bigquery_dataframe = client.query(sql).to_dataframe()
+    rows_bigquery = bigquery_dataframe['TotalRows'][0]
     percent_upload = (rows_bigquery/number_of_rows)*100
-    print(percent_upload)
+    logging.info("The percentage upload is: " , percent_upload)
 
 
-dag = DAG('covid_statewise', description='Statewise covid data',
-          schedule_interval='@daily',
-          start_date= datetime(2020, 6, 3), catchup=False)
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime.datetime(2020, 6, 1),
+    'email': ['shreya.kaushik@nineleaps.com'],
+    'email_on_failure': True,
+    'email_on_retry': False,
+    'schedule_interval': '@daily',
+    'retries': 1,
+    'retry_delay': datetime.timedelta(seconds=5),
+}
 
-dummy_operator = DummyOperator(task_id='dummy_task', retries=3,
-                               dag=dag)
+dag = DAG('covid_statewise',
+          description='Statewise covid data',
+          catchup=False,
+          default_args = default_args)
+
 csv_operator = PythonOperator(task_id='create_csv',
                                 python_callable=create_csv, dag=dag)
 bigquery_operator = PythonOperator(task_id='to_bigquery',
@@ -70,4 +96,5 @@ percentage_operator = PythonOperator(task_id='percentage_upload',
                                 python_callable=percentage_upload, dag=dag)
 
 
-dummy_operator >> csv_operator >> bigquery_operator >> percentage_operator
+csv_operator >> bigquery_operator >> percentage_operator
+
